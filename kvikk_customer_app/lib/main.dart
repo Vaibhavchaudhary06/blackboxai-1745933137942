73c023ede6dbe74d0a0d0f378fbf6f13267ed16b
Vaibhav Chaudhary <167176480+Vaibhavchaudhary06@users.noreply.github.com>
2025-04-29 18:55:45 +0530

    Add kvikk_customer_app/lib/main.dart 1745933144027

---
+++ kvikk_customer_app/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(KvikkApp());
+}
+
+class KvikkApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kvikk',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.yellow[700],
+        scaffoldBackgroundColor: Colors.black,
+        fontFamily: 'Roboto',
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.white),
+          bodyText2: TextStyle(color: Colors.white70),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.yellow[700],
+            onPrimary: Colors.black,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.grey[900],
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.yellow[700]!),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey[700]!),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          labelStyle: TextStyle(color: Colors.yellow[700]),
+          hintStyle: TextStyle(color: Colors.grey[500]),
+        ),
+      ),
+      home: LoginScreen(),
+      // For testing, you can change to HomepageScreen()
+      // home: HomepageScreen(),
+    );
+  }
+}
